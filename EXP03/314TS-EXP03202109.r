c58c953979123753ee2d0b6832b70e3ca2deffd3
manumaxg <104378269+manumaxg@users.noreply.github.com>
2023-05-28 20:20:27 -0300

    L

--- EXP03/314TS-EXP03202109.r
+++ EXP03/314TS-EXP03202109.r
@@ -22,11 +22,12 @@ PARAM$exp_input  <- "FE6314"
 
 # Corrida sin incluir la pandemia. 
 
-PARAM$future <- c( 202109 ) 
-PARAM$final_train <- c( 202107, 202106, 202105, 202104, 202103, 202102, 202101, 202012, 202011, 202010, 202009, 202008, 202007, 202005, 202002, 202001, 201912, 201911 )
- 
-PARAM$train$training <- c( 202105, 202104, 202103, 202102, 202101, 202012, 202011, 202010,202009,202008, 202007, 202005, 202002, 202001, 201912, 201911, 201910, 201909 ) 
-PARAM$train$validation <- c( 202106 )
+PARAM$future <- c( 202107 ) 
+PARAM$final_train <- c( 202105, 202104, 202103, 202102, 202101, 202012, 202011, 202010, 202009, 202008, 202007, 202006, 202005, 202002, 202001, 201912, 201911, 201910)
+
+PARAM$train$training <- c( 202103, 202102, 202101, 202012, 202011, 202010, 202009, 202008, 202007, 202006, 202005, 202002, 202001, 201912, 201911, 201910, 201909, 201908) 
+PARAM$train$validation <- c( 202104 )
+PARAM$train$testing <- c( 202105 )
 
 # Atencion  0.4  de  undersampling de la clase mayoritaria,  los CONTINUA
 PARAM$train$undersampling  <- 0.4   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
